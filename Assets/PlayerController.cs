b78ccfe1aa189f69d2f4f560ee187770dc6777a3
NZig618 <94143570+NZig618@users.noreply.github.com>
2025-07-16 20:39:09 -0400

    [Implementation] Player rotates to follow mouse

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -7,18 +7,27 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 5f;
+    public float rotSpeed = 5f;
     public Rigidbody2D rb;
 
     Vector2 moveDirection;
 
     [SerializeField]
-    private InputActionReference movement;
+    private InputActionReference movement, pointerPosition, attack;
 
     // Update is called once per frame
     void Update()
     {
         moveDirection = movement.action.ReadValue<Vector2>().normalized;
-        rb.linearVelocity = new Vector2 (moveDirection.x * moveSpeed, moveDirection.y * moveSpeed); 
+        rb.linearVelocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
+        OrientMouse();
+    }
+
+    void OrientMouse()
+    {
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(pointerPosition.action.ReadValue<Vector2>());
+        Vector2 orientation = mousePos - rb.position;
+        rb.rotation = Mathf.Atan2(orientation.y, orientation.x) * Mathf.Rad2Deg - 90f;
     }
 
 }
